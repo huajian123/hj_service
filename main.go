4f78973b74e9b9a13bdc903cf74280f10d8b0c63
huajian <287643967@qq.com>
2020-05-02 16:59:18 +0800

    初始项目结构

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	// app := iris.New() // 实例一个iris对象
+
+}
+
+func newApp() *iris.Application {
+	app := iris.New()
+
+	return app
+}
