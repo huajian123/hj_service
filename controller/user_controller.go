87c768c22f873666321e45dfb8b4daf608ea0a8b
huajian <287643967@qq.com>
2020-05-03 17:22:34 +0800

    初始项目结构

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,10 +2,21 @@ package controller
 
 import (
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/mvc"
 	"hj_service/services"
 )
 
-type LoginController struct {
+type UserController struct {
 	Ctx     iris.Context
 	Service services.UserService
+}
+
+func (userController *UserController) BeforeActivation(a mvc.BeforeActivation) {
+	a.Handle("GET", "/query", "UserInfo")
+}
+
+func (uc *UserController) UserInfo() mvc.Result {
+	//todo
+	iris.New().Logger().Info(" user info query ")
+	return mvc.Response{}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,13 @@
 package routes
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/mvc"
+	"hj_service/controller"
+)
 
-func App(api *iris.Application) {
-
+func App(app *iris.Application) {
+	mvc.Configure(app.Party("/user"), func(context *mvc.Application) {
+		context.Handle(new(controller.UserController))
+	})
 }
